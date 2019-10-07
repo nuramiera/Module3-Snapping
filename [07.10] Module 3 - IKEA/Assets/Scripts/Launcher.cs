a6b3a520b22f4973634b47f15b1c9eeae4b6d151
nuramiera <nuramiera@users.github.com>
2019-10-07 15:54:39 +0800

    Upload Files

---
+++ [07.10] Module 3 - IKEA/Assets/Scripts/Launcher.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public GameObject ConnectedScreen;
+    public GameObject DisconnectScreen;
+
+    string roomName = "My Room";
+    bool ShowGUI = true;
+
+
+
+
+    public void OnClick_ConnectBtn()
+    {
+
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+
+    public override void OnConnectedToMaster()
+    {
+    
+
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectScreen.SetActive(true);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        if (DisconnectScreen.activeSelf)
+            DisconnectScreen.SetActive(false);
+
+        ConnectedScreen.SetActive(true);
+
+
+    }
+}
---
+++ [07.10] Module 3 - IKEA/Assets/VR-Socket-master/Assets/Script/Connection/Manager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviourPunCallbacks
+{
+
+    #region Public Fields
+
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+    public Transform spawnPoint;
+
+
+    //public static Manager Instance;
+
+    #endregion
+
+    #region Public Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.CurrentRoom == null)
+        {
+           Debug.Log("return to lobby");
+           UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+            return;
+        }
+        //Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+        // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+
+
+
+        PhotonNetwork.Instantiate(this.playerPrefab.name, spawnPoint.position, Quaternion.identity, 0);
+
+
+        //else
+        //{
+
+        //Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+
+
+    }
+
+    public void LeaveRoom()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+
+    #endregion
+}
+
+
+
